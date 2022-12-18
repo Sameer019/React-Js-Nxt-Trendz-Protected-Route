05d132cb68050b95d13c9d9f611153fb9a9926c9
Sameer019 <mohammadsameer0532@yahoo.com>
2022-12-18 04:02:50 +0000

    first commit

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import {Route, Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const token = Cookies.get('jwt_token')
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  // FIX1: the spread operator should be used while sending the props
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
